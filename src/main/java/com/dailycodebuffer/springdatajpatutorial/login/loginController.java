69b5964f67275270264cfd803e2083f3bcee1b71
mohit <mohitvishwakarma6jan@gmail.com>
2024-02-02 23:01:01 +0530

    delete login package

--- src/main/java/com/dailycodebuffer/springdatajpatutorial/login/loginController.java
+++
@@ -1,9 +0,0 @@
-package com.dailycodebuffer.springdatajpatutorial.login;
-
-public class loginController {
-	
-public static void main(String[] args) {
-	System.out.println("this is login module........");
-}
-
-}
